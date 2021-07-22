437138ad66b3c78708e98ad1ef623d8caf40409d
imchanyang <cksdid636@naver.com>
2021-07-23 03:39:36 +0900

    second commit

--- Pandas0.py
+++ Pandas0.py
@@ -14,3 +14,15 @@ array = pd.Series(['사과', '바나나', '당근'], index =['a', 'b', 'c'])
 
 print(array)
 print(array['a'])
+
+
+# Dick 자료형을 Series로 바꾸기
+data = {
+    'a': '사과',
+    'b': '바나나',
+    'c': '당근'
+}
+
+array = pd.Series(data)
+print(array)
+print(array['a'])
